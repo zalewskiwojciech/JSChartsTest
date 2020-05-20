72eaaee59485775094dbc4695b8fd3df99c118d1
zalewskiwojciech <xiehu43@gmail.com>
2020-05-20 10:23:52 +0200

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+  public class HomeController : Controller
+	{
+		// GET: Home
+		public ActionResult Index()
+		{
+			List<DataPoint> dataPoints = new List<DataPoint>{
+				new DataPoint(10, 22),
+				new DataPoint(20, 36),
+				new DataPoint(30, 42),
+				new DataPoint(40, 51),
+				new DataPoint(50, 46),
+			};
+ 
+			ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+ 
+			return View();
+		}
+ 
+ 
+	}
+}
